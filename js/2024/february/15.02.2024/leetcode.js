6e5c5cdb092a5eaae190512eee51654248df540f
Abrorxon-abi <ppvbuyppv@gmail.com>
2024-02-15 11:06:48 +0500

    solved 15.02.2024 days puzzle

---
+++ js/2024/february/15.02.2024/leetcode.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var largestPerimeter = function (nums) {
+  nums.sort((a, b) => a - b);
+  let sum = nums.reduce((acc, val) => acc + val, 0);
+  let n = nums.length;
+  for (let i = n - 1; i >= 2; i--) {
+    sum -= nums[i];
+    if (sum > nums[i]) {
+      return sum + nums[i];
+    }
+  }
+  return -1;
+};
