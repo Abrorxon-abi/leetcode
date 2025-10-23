343e2c52ba2b7f31b0b709fb40fde89d5450a042
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-10-23 13:30:39 +0500

    deleted all old solutions and started posting my leetcode solutions again

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # leetcode
-<p>This is a collection of daily leetcode problems.</p>
-<p>Problems are solved in python and js.</p>
+<p>This is a collection of leetcode problems and their solutions.</p>
 <strong style="color:red">Note:</strong>
 <p>Solutions may not be cleaned or optimized. Just uploading my solutions</p>
---
+++ ts/1.ts
@@ -0,0 +1,11 @@
+// Problem Statement: https://leetcode.com/problems/two-sum/description/
+
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
