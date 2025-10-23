15d480006f79e5d2649df35302f7d239d9403255
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-10-23 13:37:44 +0500

    11

---
+++ ts/11.ts
@@ -0,0 +1,19 @@
+// Problem Statement: https://leetcode.com/problems/container-with-most-water/description/
+
+function maxArea(height: number[]): number {
+  let left = 0;
+  let right = height.length - 1;
+  let max = 0;
+  while (left < right) {
+    const area = (right - left) * Math.min(height[left], height[right]);
+    max = Math.max(max, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return max;
+}
