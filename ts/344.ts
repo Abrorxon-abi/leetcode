e7ef9406e0c10d615f30dc5041a82f48d6f2dd35
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-10-25 16:25:40 +0500

    344

---
+++ ts/344.ts
@@ -0,0 +1,8 @@
+// Problem Statement: https://leetcode.com/problems/reverse-string/description/
+
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  s.reverse();
+}
