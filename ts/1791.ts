1680c7342f491448c72cb46f3ef750d10c01ad05
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-02 23:33:27 +0500

    1791

---
+++ ts/1791.ts
@@ -0,0 +1,9 @@
+function findCenter(edges: number[][]): number {
+  const flattedArr = edges.flat();
+  const obj = {};
+
+  for (let i = 0; i < flattedArr.length; i++) {
+    obj[flattedArr[i]] = (obj[flattedArr[i]] || 0) + 1;
+    if (obj[flattedArr[i]] === 2) return flattedArr[i];
+  }
+}
