74be47dc58ef094741081db30eaeed4b25710655
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-09 16:15:42 +0500

    2535

---
+++ ts/2535.ts
@@ -0,0 +1,29 @@
+function differenceOfSum(nums: number[]): number {
+  const num1 = nums.reduce((acc, n) => (acc += n), 0);
+
+  let num2 = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 9) {
+      num2 += getDigitsMathematically(nums[i]).reduce(
+        (acc, n) => (acc += n),
+        0
+      );
+    } else {
+      num2 += nums[i];
+    }
+  }
+
+  return Math.abs(num1 - num2);
+}
+
+function getDigitsMathematically(number) {
+  const digits = [];
+
+  while (number > 0) {
+    const lastDigit = number % 10;
+    digits.unshift(lastDigit);
+    number = Math.floor(number / 10);
+  }
+  return digits;
+}
