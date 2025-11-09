56d2321dbcba15ea8eb0702919b0c42eb8cb3c8a
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-09 15:47:16 +0500

    3285

---
+++ ts/3285.ts
@@ -0,0 +1,10 @@
+function stableMountains(height: number[], threshold: number): number[] {
+  const ans = [];
+  for (let i = 1; i < height.length; i++) {
+    if (height[i - 1] > threshold) {
+      ans.push(i);
+    }
+  }
+
+  return ans;
+}
