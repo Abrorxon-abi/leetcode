327152b3c40514f80da06db735c9c214bd937151
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-09 15:48:54 +0500

    3162

---
+++ ts/3162.ts
@@ -0,0 +1,13 @@
+function numberOfPairs(nums1: number[], nums2: number[], k: number): number {
+  let acc = 0;
+
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] % (nums2[j] * k) === 0) {
+        acc++;
+      }
+    }
+  }
+
+  return acc;
+}
