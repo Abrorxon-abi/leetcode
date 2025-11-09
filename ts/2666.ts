7cadfe75ce694d62ad877b248abb8b95d0e3e19a
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-09 16:04:13 +0500

    2666

---
+++ ts/2666.ts
@@ -0,0 +1,25 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
+
+function once(fn: Function): OnceFn {
+  let calls = 0;
+  return function (...args) {
+    calls++;
+    if (calls > 1) return undefined;
+    return fn(...args);
+  };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
