798b635f2311416d0a39b22e233a4220291ae271
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-09 17:25:26 +0500

    fixed ts error

--- ts/3289.ts
+++ ts/3289.ts
@@ -1,8 +1,8 @@
 function getSneakyNumbers(nums: number[]): number[] {
-  const obj = nums.reduce((acc, num) => {
+  const obj: Record<number, number> = nums.reduce((acc, num) => {
     acc[num] = (acc[num] || 0) + 1;
     return acc;
-  }, {});
+  }, {} as Record<number, number>);
 
   const numbersWithValueTwo = Object.entries(obj)
     .filter(([_, count]) => count === 2)
