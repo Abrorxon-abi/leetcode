533d705d1225622515879e8cc6a1f19aa48f6123
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-09 16:56:06 +0500

    fixed ts warning

--- ts/1791.ts
+++ ts/1791.ts
@@ -1,9 +1,11 @@
 function findCenter(edges: number[][]): number {
   const flattedArr = edges.flat();
-  const obj = {};
+  const obj: Record<number, number> = {};
 
   for (let i = 0; i < flattedArr.length; i++) {
     obj[flattedArr[i]] = (obj[flattedArr[i]] || 0) + 1;
     if (obj[flattedArr[i]] === 2) return flattedArr[i];
   }
+
+  return -1;
 }
