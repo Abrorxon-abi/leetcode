1ad3e9a7bcdebb45cf04f4f8b070e78bc9746ff0
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-09 15:48:28 +0500

    3194

---
+++ ts/3194.ts
@@ -0,0 +1,11 @@
+function minimumAverage(nums: number[]): number {
+  const avgs = [];
+
+  nums.sort((a, b) => a - b);
+
+  while (nums.length != 0) {
+    avgs.push((nums.pop() + nums.shift()) / 2);
+  }
+
+  return Math.min(...avgs);
+}
