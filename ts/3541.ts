7c1ef4376658819c802b67de9f120cca606fbb7b
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-09 15:43:10 +0500

    3541

---
+++ ts/3541.ts
@@ -0,0 +1,22 @@
+function maxFreqSum(s: string): number {
+  const vowels = "aoeui";
+  const obj = {};
+
+  for (const char of s) {
+    obj[char] = (obj[char] || 0) + 1;
+  }
+
+  const entries = Object.entries(obj) as [string, number][];
+
+  entries.sort((a, b) => b[1] - a[1]);
+
+  const filteredVowels = entries.filter((arr) => {
+    return vowels.includes(arr[0]);
+  });
+
+  const filteredConsonants = entries.filter((arr) => {
+    return !vowels.includes(arr[0]);
+  });
+
+  return (filteredConsonants[0]?.[1] || 0) + (filteredVowels[0]?.[1] || 0);
+}
