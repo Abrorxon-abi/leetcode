562e0c984fc41b02e7d69b59ac0dbf5a4d1d050a
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-09 16:45:52 +0500

    fixed ts warning

--- ts/136.ts
+++ ts/136.ts
@@ -1,7 +1,7 @@
 // Problem Statement: https://leetcode.com/problems/single-number/description/
 
 function singleNumber(nums: number[]): number {
-  let num;
+  let num = 0;
   for (let i = 0; i < nums.length; i++) {
     num ^= nums[i];
   }
