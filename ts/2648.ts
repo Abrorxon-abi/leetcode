95495b7ad0420708c17269730341a85d0f384319
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-10 11:17:38 +0500

    2648

---
+++ ts/2648.ts
@@ -0,0 +1,16 @@
+function* fibGenerator(): Generator<number, any, number> {
+  let a = 0;
+  let b = 1;
+  while (true) {
+    yield a;
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+}
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
