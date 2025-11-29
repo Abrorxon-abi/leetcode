567a6c0b9e1f1005340dc6dfd27db1a04f18d116
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-11-29 10:03:26 +0500

    2460

---
+++ ts/2460.ts
@@ -0,0 +1,18 @@
+function applyOperations(nums: number[]): number[] {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums[i] = nums[i] * 2;
+      nums[i + 1] = 0;
+    }
+  }
+
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      j++;
+    }
+  }
+
+  return nums;
+}
