eb2e362707a3804dab8b97d19852c8194a528271
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-12-12 09:11:56 +0500

    2176

---
+++ ts/2176.ts
@@ -0,0 +1,13 @@
+function countPairs(nums: number[], k: number): number {
+  let counter = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j] && (i * j) % k === 0) {
+        counter++;
+      }
+    }
+  }
+
+  return counter;
+}
