b4963ff77f0db3e912984d30f9bee8978bf006db
Abrorxon-abi <ppvbuyppv@gmail.com>
2026-01-14 10:46:05 +0500

    2715

---
+++ ts/2715.ts
@@ -0,0 +1,38 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void;
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+  let willFnCall = true;
+  setTimeout(() => willFnCall && fn(...args), t);
+  return () => (willFnCall = false);
+}
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
