12cad3f29235f35b8e966a8c62abbb4d89bc43d8
Abrorxon-abi <ppvbuyppv@gmail.com>
2026-03-03 22:47:45 +0500

    3019

---
+++ ts/3019.ts
@@ -0,0 +1,10 @@
+function countKeyChanges(s: string): number {
+  s = s.toLowerCase();
+  let counter = 0;
+
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] != s[i + 1]) counter++;
+  }
+
+  return counter;
+}
