46aed87b48b78964dafa75a91e47cf6b705ea4e0
Abrorxon-abi <ppvbuyppv@gmail.com>
2026-03-09 06:09:38 +0500

    2951

---
+++ ts/2951.ts
@@ -0,0 +1,11 @@
+function findPeaks(mountain: number[]): number[] {
+  const ans = [];
+
+  for (let i = 1; i < mountain.length - 1; i++) {
+    if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
+      ans.push(i);
+    }
+  }
+
+  return ans;
+}
